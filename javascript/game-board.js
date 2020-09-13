d51881dd597404ff53fe77c9ad10c44b10e903aa
gheezl <gheezl25@gmail.com>
2020-09-13 16:14:38 -0400

    Added a remove image feature

--- javascript/game-board.js
+++ javascript/game-board.js
@@ -27,9 +27,22 @@ let clickCounter = 0
 // this hides all the images after 1 second of display
 
 const hideImages = () => {
-    for (let item of allImages) {
-        item.style.opacity = "0"
-    }
+    clickCounter = 0
+    clickedImages = []
+    // for (let item of allImages) {
+    //     item.style.opacity = "0"
+    // }
+}
+
+// this deletes the matching images
+
+const deleteImages = () => {
+    console.log(clickedImages[0])
+    const matchingImage1 = document.getElementById(clickedImages[0][1])
+    const matchingImage2 = document.getElementById(clickedImages[1][1])
+    matchingImage1.remove()
+    matchingImage2.remove()
+    clickedImages = []
 }
 
 // this displays the score on the header
@@ -42,12 +55,14 @@ const drawImage = (gameBoard) => {
     // this creates a random image
 
     const imageBorder = document.createElement("div")
+    imageBorder.id = Math.random()
     imageBorder.classList.add("img-border")
+
     const imageElement = document.createElement("img")
-    imageElement.style.gridRowStart = Math.floor(Math.random() * 1)
-    imageElement.style.gridColumnStart = Math.floor(Math.random() * 1)
+    imageElement.style.gridRowStart = Math.floor(Math.random() * Math.random())
+    imageElement.style.gridColumnStart = Math.floor(Math.random() * Math.random())
     imageElement.classList.add("img")
-    imageElement.style.opacity = "0"
+    // imageElement.style.opacity = "0"
     imageElement.src = getRandomImage(images)
 
     // this is the function that displays the image after it is clicked
@@ -55,21 +70,17 @@ const drawImage = (gameBoard) => {
     const displayImage = () => {
         clickCounter += 1
         imageElement.style.opacity = "1"
-        clickedImages.push(imageElement.src)
+        clickedImages.push([imageElement.src, imageBorder.id])
+        console.log(clickedImages, clickCounter)
 
-        if (clickedImages[0] === clickedImages[1]) {
-            clickedImages = []
+        if (clickedImages[0][0] === clickedImages[1][0]) {
+            setTimeout(deleteImages, 1000)
             score = score + 1
             scoreDocument.innerHTML = score
         }
 
-        if (clickedImages.length === 2) {
-            clickedImages = []
-        }
-
         if (clickCounter === 2) {
-            clickCounter = 0
-            interval = setTimeout(hideImages, 1000)
+            setTimeout(hideImages, 1000)
         }
 
         return;
