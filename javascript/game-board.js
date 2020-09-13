4fd402d78f8d32641bab4efaa90378f6a04a4269
gheezl <gheezl25@gmail.com>
2020-09-13 16:39:38 -0400

    Working on solving the grid location bug

--- javascript/game-board.js
+++ javascript/game-board.js
@@ -5,7 +5,21 @@ const images = [
     "https://www.drupal.org/files/project-images/github_commits_logo.png",
     "https://image.freepik.com/free-icon/github-logo-in-a-rounded-square_318-40761.jpg",
     "http://www.pngall.com/wp-content/uploads/2016/04/Github-PNG-Image.png",
+    "https://cdn.afterdawn.fi/v3/news/original/github-logo.png",
+    "http://static.movingpackets.net/2013/12/github-logo-transparent.jpg",
+    "http://static.movingpackets.net/2013/12/github-logo-transparent.jpg",
+    "https://www.drupal.org/files/project-images/github_commits_logo.png",
+    "https://image.freepik.com/free-icon/github-logo-in-a-rounded-square_318-40761.jpg",
+    "http://www.pngall.com/wp-content/uploads/2016/04/Github-PNG-Image.png",
+    "https://cdn.afterdawn.fi/v3/news/original/github-logo.png",
+    "http://static.movingpackets.net/2013/12/github-logo-transparent.jpg",
+    "http://static.movingpackets.net/2013/12/github-logo-transparent.jpg",
+    "https://www.drupal.org/files/project-images/github_commits_logo.png",
+    "https://image.freepik.com/free-icon/github-logo-in-a-rounded-square_318-40761.jpg",
+    "http://www.pngall.com/wp-content/uploads/2016/04/Github-PNG-Image.png",
 ]
+
+
 
 // this function randomly selects an image
 
@@ -23,6 +37,8 @@ let clickedImages = []
 let score = 0
 let interval = null
 let clickCounter = 0
+let rowPosition = 0
+let columnPosition = 0
 
 // this hides all the images after 1 second of display
 
@@ -37,12 +53,13 @@ const hideImages = () => {
 // this deletes the matching images
 
 const deleteImages = () => {
-    console.log(clickedImages[0])
     const matchingImage1 = document.getElementById(clickedImages[0][1])
     const matchingImage2 = document.getElementById(clickedImages[1][1])
     matchingImage1.remove()
     matchingImage2.remove()
     clickedImages = []
+    score = score + 1
+    scoreDocument.innerHTML = score
 }
 
 // this displays the score on the header
@@ -52,15 +69,22 @@ scoreDocument.innerHTML = score.toString()
 // this is the function that draws the card that holds the image
 
 const drawImage = (gameBoard) => {
+    columnPosition += 1
+
+    if (columnPosition === 10) {
+        rowPosition += 1
+        columnPosition = 0
+    }
+
     // this creates a random image
 
     const imageBorder = document.createElement("div")
     imageBorder.id = Math.random()
     imageBorder.classList.add("img-border")
+    imageBorder.style.gridRowStart = rowPosition
+    imageBorder.style.gridColumnStart = columnPosition
 
     const imageElement = document.createElement("img")
-    imageElement.style.gridRowStart = Math.floor(Math.random() * Math.random())
-    imageElement.style.gridColumnStart = Math.floor(Math.random() * Math.random())
     imageElement.classList.add("img")
     // imageElement.style.opacity = "0"
     imageElement.src = getRandomImage(images)
@@ -71,12 +95,9 @@ const drawImage = (gameBoard) => {
         clickCounter += 1
         imageElement.style.opacity = "1"
         clickedImages.push([imageElement.src, imageBorder.id])
-        console.log(clickedImages, clickCounter)
 
         if (clickedImages[0][0] === clickedImages[1][0]) {
             setTimeout(deleteImages, 1000)
-            score = score + 1
-            scoreDocument.innerHTML = score
         }
 
         if (clickCounter === 2) {
